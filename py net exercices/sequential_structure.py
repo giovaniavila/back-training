3a95ae8adfaecc6246405968481581c2ece82be6
giovani <giovanicarvalhoavila@hotmail.com>
2022-11-02 10:51:51 -0300

    :tada: py init net exercicices :tada:

---
+++ py net exercices/sequential_structure.py
@@ -0,0 +1,37 @@
+print('alô mundo')
+
+num = int(input('número: '))
+print(f'número informado é {num}')
+
+n1 = int(input('primeiro número: '))
+n2 = int(input('segundo número: '))
+print(n1+n2)
+
+b1 = int(input('bimestre 1: '))
+b2 = int(input('bimestre 2: '))
+b3 = int(input('bimestre 3: '))
+b4 = int(input('bimestre 4: '))
+print(f'a média é {(b1+b2+b3+b4)/4}')
+
+metro = int(input('digite em metros: '))
+print(f'em centímetros é:{metro*100}cm')
+
+raio = int(input('raio do círculo: '))
+print(f'a área do círculo é aproximadamente de:{(raio**2)*3,14}')
+
+quadrado = int(input('digite a área do quadrado: '))
+print(f'o dobro da área é de {quadrado*2}')
+
+hora = int(input('quanto você ganha por hora: '))
+trabalhadas = int(input('número de horas trabalhadas: '))
+print(f'seu salário é de R${hora*trabalhadas}')
+
+temperatura = int(input('digite a temperatura em graus fahrenheint: '))
+print(f'temperatura em graus celsius é de {(5*(temperatura-32))/9}')
+
+n1 = int(input('digite um número inteiro: '))
+n2 = float(input('digite um número real: '))
+print(f'{(n1*2)*(n2/2)}')
+print(f'{(n1*3)+(n1*3)}')
+print(f'{n1**3}')
+
