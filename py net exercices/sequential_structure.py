04ac0e72f698fd7e98462990839f2372930ecd4f
giovani <giovanicarvalhoavila@hotmail.com>
2022-11-04 20:36:59 -0300

    :construction: py exercices sequential structure

--- py net exercices/sequential_structure.py
+++ py net exercices/sequential_structure.py
@@ -48,3 +48,25 @@ print(f'{(n1*2)*(n2/2)}')
 print(f'{(n1*3)+(n1*3)}')
 print(f'{n1**3}')
 
+
+#Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00. Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+#comprar apenas latas de 18 litros;
+#comprar apenas galões de 3,6 litros;
+#misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
+
+metro = float(input('tamanho da área a ser pintada:'))
+
+litros = 6*metro
+
+lata = litros/18
+galao = litros/3.6
+
+pl = lata*80
+pg = galao*25
+
+print(f'comprando latas:R${pl}')
+print(f'comprando galao:R${pg}')
+
+
+
+
