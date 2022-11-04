f6f66a76aaa7676122c52660d95362497a502a46
giovani <giovanicarvalhoavila@hotmail.com>
2022-11-04 20:50:01 -0300

    :construction: py exercices sequencial structure

--- py net exercices/sequential_structure.py
+++ py net exercices/sequential_structure.py
@@ -25,34 +25,25 @@ print(f'em centímetros é:{metro*100}cm')
 raio = int(input('raio do círculo: '))
 print(f'a área do círculo é aproximadamente de:{(raio**2)*3,14}')
 
-#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
 quadrado = int(input('digite a área do quadrado: '))
 print(f'o dobro da área é de {quadrado*2}')
 
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês
+
 hora = int(input('quanto você ganha por hora: '))
 trabalhadas = int(input('número de horas trabalhadas: '))
 print(f'seu salário é de R${hora*trabalhadas}')
 
-#Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius
+
 temperatura = int(input('digite a temperatura em graus fahrenheint: '))
 print(f'temperatura em graus celsius é de {(5*(temperatura-32))/9}')
 
-#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
-#o produto do dobro do primeiro com metade do segundo .
-#a soma do triplo do primeiro com o terceiro.
-#o terceiro elevado ao cubo
 n1 = int(input('digite um número inteiro: '))
 n2 = float(input('digite um número real: '))
 print(f'{(n1*2)*(n2/2)}')
 print(f'{(n1*3)+(n1*3)}')
 print(f'{n1**3}')
 
-
-#Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00. Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
-#comprar apenas latas de 18 litros;
-#comprar apenas galões de 3,6 litros;
-#misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
 metro = float(input('tamanho da área a ser pintada:'))
 
@@ -64,8 +55,8 @@ galao = litros/3.6
 pl = lata*80
 pg = galao*25
 
-print(f'comprando latas:R${pl}')
-print(f'comprando galao:R${pg}')
+print(f'comprando latas: R${pl}')
+print(f'comprando galao: R${pg}')
 
 
 
